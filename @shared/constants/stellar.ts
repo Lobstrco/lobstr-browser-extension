ab16f4c9b07dec74f4463970bdeec3b600b7192c
flat-resonance <44231988+flat-resonance@users.noreply.github.com>
2024-11-21 14:39:01 +0200

    Version updated

--- @shared/constants/stellar.ts
+++ @shared/constants/stellar.ts
@@ -2,8 +2,5 @@ export const ASSETS_URL = "https://assets.ultrastellar.com/api/v1/assets/";
 
 export const HORIZON_URL = "https://horizon.stellar.lobstr.co";
 
-export const NATIVE_PRICES_URL =
-  "https://markets.ultrastellar.com/api/v1/assets/native-prices/";
-
 export const ALTERNATIVE_PRICES_URL =
-    "https://staging.lobstr.co/api/v2/assets/alternative-rates/";
+  "https://lobstr.co/api/v2/assets/alternative-rates/";
